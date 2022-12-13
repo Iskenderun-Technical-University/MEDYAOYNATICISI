0980ad11e39b623c2f4c924b906a5cace44b0ea3
yusufsariduman <yusufsariduman.mdbf21@iste.edu.tr>
2022-12-13 22:19:48 +0300

    tasarım

--- MEDYAOYNATICISI/Form1.Designer.cs
+++ MEDYAOYNATICISI/Form1.Designer.cs
@@ -29,11 +29,352 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.paneltop = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panelvideo = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.button7 = new System.Windows.Forms.Button();
+            this.ımageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.paneltop.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel7.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // paneltop
+            // 
+            this.paneltop.BackColor = System.Drawing.Color.MintCream;
+            this.paneltop.Controls.Add(this.panel2);
+            this.paneltop.Controls.Add(this.label1);
+            this.paneltop.Controls.Add(this.button3);
+            this.paneltop.Controls.Add(this.button2);
+            this.paneltop.Controls.Add(this.button1);
+            this.paneltop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.paneltop.Location = new System.Drawing.Point(0, 0);
+            this.paneltop.Name = "paneltop";
+            this.paneltop.Padding = new System.Windows.Forms.Padding(2);
+            this.paneltop.Size = new System.Drawing.Size(1075, 36);
+            this.paneltop.TabIndex = 0;
+            this.paneltop.MouseDown += new System.Windows.Forms.MouseEventHandler(this.paneltop_MouseDown);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(836, 51);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(250, 535);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(466, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.MintCream;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.button3.ImageIndex = 7;
+            this.button3.ImageList = this.ımageList1;
+            this.button3.Location = new System.Drawing.Point(995, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(36, 36);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList1.Images.SetKeyName(0, "841-8414490_13-minimize-maximize-icon-images-minimize-png.png");
+            this.ımageList1.Images.SetKeyName(1, "27185.png");
+            this.ımageList1.Images.SetKeyName(2, "64595.png");
+            this.ımageList1.Images.SetKeyName(3, "close.png");
+            this.ımageList1.Images.SetKeyName(4, "fullscreen (1).png");
+            this.ımageList1.Images.SetKeyName(5, "fullscreen.png");
+            this.ımageList1.Images.SetKeyName(6, "fullscreen8.png");
+            this.ımageList1.Images.SetKeyName(7, "fullscreir.png");
+            this.ımageList1.Images.SetKeyName(8, "indir.png");
+            this.ımageList1.Images.SetKeyName(9, "oie_mM6Jv6K9BUIn.png");
+            this.ımageList1.Images.SetKeyName(10, "png-clipart-computer-icons-arrow-previous-button-angle-rectangle-thumbnail.png");
+            this.ımageList1.Images.SetKeyName(11, "png-red-round-close-x-icon-31631915146jpppmdzihs.png");
+            this.ımageList1.Images.SetKeyName(12, "png-transparent-volume-icon-volume-computer-icons-sound-icon-volume-miscellaneous" +
+        "-text-hand-thumbnail.png");
+            this.ımageList1.Images.SetKeyName(13, "sound-off-music-mute-off-sound-speaker-volume-icon-16.png");
+            this.ımageList1.Images.SetKeyName(14, "ters.png");
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.MintCream;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.button2.ImageIndex = 0;
+            this.button2.ImageList = this.ımageList1;
+            this.button2.Location = new System.Drawing.Point(949, -1);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(40, 37);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MintCream;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.button1.ImageIndex = 11;
+            this.button1.ImageList = this.ımageList1;
+            this.button1.Location = new System.Drawing.Point(1037, -1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(38, 37);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel4.Controls.Add(this.listBox1);
+            this.panel4.Controls.Add(this.panel5);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel4.Location = new System.Drawing.Point(825, 36);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(250, 556);
+            this.panel4.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(0, 34);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(250, 460);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.MintCream;
+            this.panel5.Controls.Add(this.label2);
+            this.panel5.Location = new System.Drawing.Point(0, 1);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(250, 40);
+            this.panel5.TabIndex = 0;
+            this.panel5.Paint += new System.Windows.Forms.PaintEventHandler(this.panel5_Paint);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Oynatma Listesi";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.panelvideo);
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel6.Location = new System.Drawing.Point(0, 36);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(825, 485);
+            this.panel6.TabIndex = 2;
+            this.panel6.Paint += new System.Windows.Forms.PaintEventHandler(this.panel6_Paint);
+            // 
+            // panelvideo
+            // 
+            this.panelvideo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelvideo.Location = new System.Drawing.Point(30, 11);
+            this.panelvideo.Name = "panelvideo";
+            this.panelvideo.Size = new System.Drawing.Size(751, 461);
+            this.panelvideo.TabIndex = 0;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel7.Controls.Add(this.button7);
+            this.panel7.Controls.Add(this.trackBar1);
+            this.panel7.Controls.Add(this.button6);
+            this.panel7.Controls.Add(this.button5);
+            this.panel7.Controls.Add(this.button4);
+            this.panel7.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel7.Location = new System.Drawing.Point(0, 521);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(825, 69);
+            this.panel7.TabIndex = 3;
+            this.panel7.Paint += new System.Windows.Forms.PaintEventHandler(this.panel7_Paint);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button7.FlatAppearance.BorderSize = 0;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.button7.ImageIndex = 13;
+            this.button7.ImageList = this.ımageList2;
+            this.button7.Location = new System.Drawing.Point(622, 11);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(50, 47);
+            this.button7.TabIndex = 11;
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // ımageList2
+            // 
+            this.ımageList2.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.ımageList2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList2.ImageStream")));
+            this.ımageList2.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList2.Images.SetKeyName(0, "841-8414490_13-minimize-maximize-icon-images-minimize-png.png");
+            this.ımageList2.Images.SetKeyName(1, "27185.png");
+            this.ımageList2.Images.SetKeyName(2, "64595.png");
+            this.ımageList2.Images.SetKeyName(3, "close.png");
+            this.ımageList2.Images.SetKeyName(4, "fullscreen (1).png");
+            this.ımageList2.Images.SetKeyName(5, "fullscreen.png");
+            this.ımageList2.Images.SetKeyName(6, "fullscreen8.png");
+            this.ımageList2.Images.SetKeyName(7, "fullscreir.png");
+            this.ımageList2.Images.SetKeyName(8, "indir.png");
+            this.ımageList2.Images.SetKeyName(9, "oie_mM6Jv6K9BUIn.png");
+            this.ımageList2.Images.SetKeyName(10, "png-clipart-computer-icons-arrow-previous-button-angle-rectangle-thumbnail.png");
+            this.ımageList2.Images.SetKeyName(11, "png-red-round-close-x-icon-31631915146jpppmdzihs.png");
+            this.ımageList2.Images.SetKeyName(12, "png-transparent-volume-icon-volume-computer-icons-sound-icon-volume-miscellaneous" +
+        "-text-hand-thumbnail.png");
+            this.ımageList2.Images.SetKeyName(13, "sound-off-music-mute-off-sound-speaker-volume-icon-16.png");
+            this.ımageList2.Images.SetKeyName(14, "ters.png");
+            this.ımageList2.Images.SetKeyName(15, "ters.png");
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(678, 12);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(130, 56);
+            this.trackBar1.TabIndex = 10;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.button6.ImageIndex = 2;
+            this.button6.ImageList = this.ımageList2;
+            this.button6.Location = new System.Drawing.Point(358, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(49, 47);
+            this.button6.TabIndex = 9;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.button5.ImageIndex = 15;
+            this.button5.ImageList = this.ımageList2;
+            this.button5.Location = new System.Drawing.Point(466, 11);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(50, 47);
+            this.button5.TabIndex = 8;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.button4.ImageIndex = 1;
+            this.button4.ImageList = this.ımageList2;
+            this.button4.Location = new System.Drawing.Point(413, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(50, 46);
+            this.button4.TabIndex = 7;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1075, 592);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.paneltop);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Medya Oynatıcısı 2bin";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.paneltop.ResumeLayout(false);
+            this.paneltop.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel6.ResumeLayout(false);
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private Button button1;
+        private ImageList ımageList1;
+        private Button button3;
+        private Button button2;
+        private Panel panel2;
+        private Label label1;
+        private Panel paneltop;
+        private Panel panel4;
+        private Panel panel5;
+        private Label label2;
+        private ListBox listBox1;
+        private Panel panel6;
+        private Panel panel7;
+        private Panel panelvideo;
+        private ImageList ımageList2;
+        private Button button4;
+        private Button button6;
+        private Button button5;
+        private Button button7;
+        private TrackBar trackBar1;
     }
 }
